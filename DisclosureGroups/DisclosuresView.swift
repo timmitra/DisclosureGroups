57212d26175de17771e1cca843b7ad7c63c73f42
Tim Mitra <tim@it-guy.com>
2024-05-03 12:30:49 -0400

    add model set up disclosure view

---
+++ DisclosureGroups/DisclosuresView.swift
@@ -0,0 +1,62 @@
+//
+//  DisclosuresView.swift
+//  DisclosureGroups
+//
+//  Created by Tim Mitra on 2024-05-03.
+//
+
+import SwiftUI
+
+struct DisclosuresView: View {
+  @State private var myList: [Country] =
+  [Country(
+    name: "USA",
+    animals: [Animals(
+      name: "Turtle",
+      weight: 15
+    ),
+              Animals(
+                name: "Cat",
+                weight: 8
+              )]
+  ),
+   Country(
+    name: "Canada",
+    animals: [Animals(
+      name: "Moose",
+      weight: 990
+    ),
+              Animals(
+                name: "Beaver",
+                weight: 20
+              )]
+   ),
+   Country(
+    name: "Germany",
+    animals: [Animals(
+      name: "Chamois",
+      weight: 130
+    ),
+              Animals(
+                name: "Europian Badger",
+                weight: 37
+              )]
+   )]
+  
+    var body: some View {
+      List {
+        ForEach(myList) { oneCountry in
+          Section(oneCountry.name) {
+            ForEach(oneCountry.animals) { oneAnimal in
+              Text("\(oneAnimal.name) \(oneAnimal.weight)")
+            }
+          }
+        }
+      }
+      .listStyle(SidebarListStyle())
+    }
+}
+
+#Preview {
+    DisclosuresView()
+}
---
+++ DisclosureGroups/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  DisclosureGroups
+//
+//  Created by Tim Mitra on 2024-05-03.
+//
+
+import Foundation
+
+struct Animals: Identifiable {
+  let id = UUID()
+  var name: String
+  var weight: Int
+}
+
+struct Country: Identifiable {
+  let id = UUID()
+  var name: String
+  var animals: [Animals]
+}
